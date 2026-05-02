01408737dd5bfdcb84dbed2cbe67837e950e0b63
Dipali Dharmik <dharmikdipali9@gmail.com>
2026-05-02 11:50:14 +0530

    settings.py updated

--- shortener/settings.py
+++ shortener/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'shortener.wsgi.application'
 
 if os.getenv("DATABASE_URL"):
     DATABASES = {
-        'default': dj_database_url.parse(os.getenv("DATABASE_URL"))
-    }
+    'default': dj_database_url.parse(
+        os.environ.get("DATABASE_URL"),
+        conn_max_age=600,
+        ssl_require=True
+    )
+}
 else:
     DATABASES = {
         'default': {
